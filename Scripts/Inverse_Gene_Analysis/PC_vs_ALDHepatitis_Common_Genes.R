6fbd75de797d88d41a783551eb1c55d7292b85ba
Sannahussain <sanna504@hotmail.com>
2026-06-16 16:59:30 +0100

    Update PC_vs_ALDHepatitis_Common_Genes.R

--- Scripts/Inverse_Gene_Analysis/PC_vs_ALDHepatitis_Common_Genes.R
+++ Scripts/Inverse_Gene_Analysis/PC_vs_ALDHepatitis_Common_Genes.R
@@ -1,1 +1,10 @@
+#PC vs ALDHepatitis common genes
+#load Meta-analyses results for cross disease comparisons
 
+# filter significant genes using FDR correction 
+PCMetaA <- PCMetaA[PCMetaA$FDR < 0.05, ]
+
+ALDhepMetaA <- ALDhepMetaA[ALDhepMetaA$FDR < 0.05, ]
+
+#common genes PC vs ALD hepatitis
+PCcommon_ALDhep <- intersect(PCMetaA$Gene_symbol , ALDhepMetaA$Gene_symbol )
