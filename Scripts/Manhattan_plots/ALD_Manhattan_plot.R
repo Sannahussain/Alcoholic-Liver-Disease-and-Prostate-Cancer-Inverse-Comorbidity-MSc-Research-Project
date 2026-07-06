9d1e9c3e9e442472154e61893b9f95e0b5886306
Sannahussain <sanna504@hotmail.com>
2026-07-06 18:21:41 +0100

    Create ALD_Manhattan_plot.R

---
+++ Scripts/Manhattan_plots/ALD_Manhattan_plot.R
@@ -0,0 +1,5 @@
+# This script generates a Manhattan plot from GWAS summary statistics.
+# It visualises -log10(p-values) across genomic positions for each chromosome.
+# Genome-wide significant SNPs are highlighted.
+
+
